a9e6ba4722f67de4d7db7ec6d0f52560ba7ea65f
eddie2122 <edwinrichard2122@gmail.com>
2021-12-29 23:42:18 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About(props) {
+    return (
+        <div className="container">
+            <header className="jumbotron">
+                <h3>About Page</h3>
+            </header>
+        </div>
+    );
+}
+
+export default About;
